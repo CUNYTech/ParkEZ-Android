b939819660215ae15944d68c8db98c68d1dc944c
pasan_000 <pasangdlama@gmail.com>
2016-03-04 21:31:42 -0500

    Initial work done on the login and signup activities

---
+++ app/src/main/java/com/herokuapp/parkez/parkezfinal/MainActivity.java
@@ -0,0 +1,37 @@
+package com.herokuapp.parkez.parkezfinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login_btn = null;
+    private Button signup_btn = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login_btn = (Button) findViewById(R.id.btn_login);
+        signup_btn = (Button) findViewById(R.id.btn_signup);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signupIntent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(signupIntent);
+            }
+        });
+    }
+}
