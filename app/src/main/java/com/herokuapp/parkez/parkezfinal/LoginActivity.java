12de309427214738d8cfb3da3b56e1a21cecc3ac
Robert O'Connor <rob@oconnor.ninja>
2016-03-09 02:13:58 -0500

    get rid of IDEA files

--- app/src/main/java/com/herokuapp/parkez/parkezfinal/LoginActivity.java
+++ app/src/main/java/com/herokuapp/parkez/parkezfinal/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.herokuapp.parkez.parkezfinal;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText email; // email address
+    private EditText password; // password
+    private Button button; // le login button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.et_email);
+        password = (EditText) findViewById(R.id.et_password);
+        button = (Button) findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+}
+
+
+
+
+
+
 }
