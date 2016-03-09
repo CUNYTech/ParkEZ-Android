205d05d0f85bc7545c33becee208075f5e334172
Robert O'Connor <rob@oconnor.ninja>
2016-03-09 00:59:36 -0500

    Add getters, setters, etc

--- app/src/main/java/com/herokuapp/parkez/parkezfinal/models/ParkingLocation.java
+++ app/src/main/java/com/herokuapp/parkez/parkezfinal/models/ParkingLocation.java
@@ -10,4 +10,76 @@ public class ParkingLocation {
     private Float longitude;
     private User user;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ParkingLocation(Integer id, Float latitude, Float longitude, User user) {
+
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ParkingLocation that = (ParkingLocation) o;
+
+        if (!getId().equals(that.getId())) return false;
+        if (!getLatitude().equals(that.getLatitude())) return false;
+        if (!getLongitude().equals(that.getLongitude())) return false;
+        return getUser().equals(that.getUser());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId().hashCode();
+        result = 31 * result + getLatitude().hashCode();
+        result = 31 * result + getLongitude().hashCode();
+        result = 31 * result + getUser().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingLocation{" +
+                "id=" + id +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", user=" + user +
+                '}';
+    }
 }
--- app/src/main/java/com/herokuapp/parkez/parkezfinal/models/User.java
+++ app/src/main/java/com/herokuapp/parkez/parkezfinal/models/User.java
@@ -8,4 +8,76 @@ public class User {
     private String token; // token that we use to authenticate
     private String clientId; // client id (multiple clients can use the same token)
     private String expiry; // when this token expires
+
+    public User(String uid, String token, String clientId, String expiry) {
+        this.uid = uid;
+        this.token = token;
+        this.clientId = clientId;
+        this.expiry = expiry;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!getUid().equals(user.getUid())) return false;
+        if (!getToken().equals(user.getToken())) return false;
+        if (!getClientId().equals(user.getClientId())) return false;
+        return getExpiry().equals(user.getExpiry());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getUid().hashCode();
+        result = 31 * result + getToken().hashCode();
+        result = 31 * result + getClientId().hashCode();
+        result = 31 * result + getExpiry().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", token='" + token + '\'' +
+                ", clientId='" + clientId + '\'' +
+                ", expiry='" + expiry + '\'' +
+                '}';
+    }
 }
